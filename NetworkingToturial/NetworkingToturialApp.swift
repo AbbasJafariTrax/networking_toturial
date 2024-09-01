01315cb2292aa4bd5a92e3aa49eb45b6c29f63dc
AbbasJafariTrax <abbasjaffary33@gmail.com>
2024-09-01 08:47:07 +0430

    Initial Commit

---
+++ NetworkingToturial/NetworkingToturialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingToturialApp.swift
+//  NetworkingToturial
+//
+//  Created by Abbas Jafari on 11/06/1403 AP.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingToturialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
